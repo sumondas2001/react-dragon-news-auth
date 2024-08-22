12af9a5573559020d0e5fb1c2183c5391ab30b99
sumondas2001 <sumonchandradas1010@gmail.com>
2024-08-23 03:42:50 +0600

    left sit Navbar All Caterogy section completed

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+     const [categories, setCategories] = useState([]);
+     useEffect(() => {
+          fetch('categories.json')
+               .then(res => res.json())
+               .then(data => setCategories(data))
+     }, []);
      return (
-          <div>
-               <h1 className="text-xl">Left Side Nav</h1>
+          <div className="space-y-6">
+               <h1 className="text-xl font-semibold">All Category </h1>
+               <h1 className="text-lg text-center p-3 mt-6 rounded-lg bg-slate-700 mx-7">National News</h1>
+               {
+                    categories.map(category => <NavLink to={`/category/${category.id}`}
+                         className='block pl-9 text-lg font-medium'
+                         key={category.id}>
+                         {category.name}
+
+                    </NavLink>
+                    )
+               }
           </div>
      );
 };
